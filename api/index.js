89d2843ea866a8f56d6ecf2d52f14873f6acbba9
Berkan Kaygusuz <berkankaygusuz@aof.anadolu.edu.tr>
2025-08-05 00:39:30 +0300

    Fix Vercel routing configuration

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://*.vercel.app'],
+  origin: ['http://localhost:3000', 'https://*.vercel.app', 'https://*.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
